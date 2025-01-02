17e049af78189d4eaea045100541b1d28dc1ff2e
Kaushik LS <50703773+thekaushikls@users.noreply.github.com>
2025-01-02 14:36:31 +0100

    chore: added crypto util methods

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+from re import match as regex_match
+from bson import ObjectId
+from cryptography.fernet import Fernet
 from os import urandom
 from hashlib import pbkdf2_hmac
 from base64 import b64encode, b64decode
@@ -17,6 +20,20 @@ def hash_password(password: str, base64_encoded_salt: str = "") -> tuple[str, st
     base64_encoded_hash = b64encode(hashed_password).decode()
     return base64_encoded_hash, base64_encoded_salt
 
+def generate_encryption_key() -> bytes:
+    return Fernet.generate_key()
+
+def encrypt(data: str, key: bytes) -> bytes:
+    encrypted_data = Fernet(key).encrypt(data.encode())
+    return encrypted_data
+
+def decrypt(data: bytes, key: bytes) -> str:
+    decrypted_data = Fernet(key).decrypt(data).decode()
+    return decrypted_data
+
+def is_mongo_doc(doc: dict) -> bool:
+    return isinstance(doc, dict) and "_id" in doc and isinstance(doc["_id"], ObjectId)
+
 def map_mongo_id(doc: dict) -> dict:
     key = "_id"
     if(key in doc.keys()):
@@ -24,3 +41,17 @@ def map_mongo_id(doc: dict) -> dict:
         return doc
     else:
         raise TypeError("Argument is not a valid Mongo Document")
+
+def strip_email_tag(email: str) -> dict[str]:
+    pattern = r"([A-Z0-9a-z._-]+)(\+{1}([A-Z0-9a-z_-]*))?@{1}([A-Z0-9a-z._-]+)"
+    match = regex_match(pattern, email)
+    
+    if match is None:
+        raise ValueError("Not a valid email id.")
+    
+    groups = match.groups()
+    return  { 
+        "email" : "{}{}".format(groups[0], groups[3]),
+        "tag"   : groups[1]
+    }
+        
